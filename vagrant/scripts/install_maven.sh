d1e7ec6ac846f842b3dbbc5fa5f6de0b39b3f41f
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2019-07-31 00:29:27 +0200

    Fix maven installation

--- vagrant/scripts/install_maven.sh
+++ vagrant/scripts/install_maven.sh
@@ -1,4 +1,4 @@
 wget https://www-us.apache.org/dist/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz -P /tmp
 sudo tar xf /tmp/apache-maven-3.6.1-bin.tar.gz -C /opt
 sudo ln -s /opt/apache-maven-3.6.1 /opt/maven
-sudo ln -s /usr/bin/mvn /opt/maven/bin/mvn
+sudo ln -s /opt/maven/bin/mvn  /usr/bin/mvn
