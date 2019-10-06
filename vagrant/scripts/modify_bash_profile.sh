c9a0844e219e8c1a84dcd0caeac9c143e27d5ed9
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2019-10-06 04:28:35 +0200

    fix

--- vagrant/scripts/modify_bash_profile.sh
+++ vagrant/scripts/modify_bash_profile.sh
@@ -1,6 +1,6 @@
 . /vagrant/scripts/.env
 echo "export GIT_REPO_NAME=$GIT_REPO_NAME" > /home/vagrant/.bash_profile
-echo "export GOROOT=/usr/local/go" >> /home/vagrant/.bash_profile
+echo "export GOROOT=$GOROOT" >> /home/vagrant/.bash_profile
 echo "export GOPATH=$HOME/go" >> /home/vagrant/.bash_profile
 echo "export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/rbenv/bin:$PATH" >> /home/vagrant/.bash_profile
 echo "export LC_ALL=en_US.UTF-8" >> /home/vagrant/.bash_profile
